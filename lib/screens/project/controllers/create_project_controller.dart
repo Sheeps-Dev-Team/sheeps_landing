b2d5687afaa64240e9def040bd6e3b4463d6ff19
noah <jsk9106@gmail.com>
2023-10-27 16:26:58 +0900

    프로젝트 생성 페이지 작업

--- lib/screens/project/controllers/create_project_controller.dart
+++ lib/screens/project/controllers/create_project_controller.dart
@@ -1,15 +1,17 @@
-import 'dart:typed_data';
+import 'dart:io';
 
 import 'package:file_picker/file_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:sheeps_landing/config/constants.dart';
+import 'package:sheeps_landing/config/routes.dart';
 import 'package:sheeps_landing/data/models/project.dart';
 import 'package:sheeps_landing/data/models/user_callback.dart';
-
-import '../../../util/global_function.dart';
-import 'package:dio/dio.dart' as DIO;
+import 'package:sheeps_landing/repository/project_repository.dart';
+import 'package:sheeps_landing/util/global_function.dart';
 
 class CreateProjectController extends GetxController {
   final PageController pageController = PageController();
@@ -23,16 +25,33 @@ class CreateProjectController extends GetxController {
     '방문자 트래킹을 통해 데이터를 수집하고\n다른 랜딩 페이지와 비교해\n아이디어의 잠재적 가치를 확인할 수 있습니다.️',
     '시작을 쉽게하도록 돕고 있습니다.\n아이디어를 빠르게 검증하고 성공적인 창업의 첫걸음을 시작하세요.',
   ];
+  final List<Color> keyColorList = [
+    const Color.fromRGBO(82, 58, 133, 1),
+    Colors.indigo,
+    Colors.blue,
+    Colors.teal,
+    $style.colors.primary,
+    Colors.yellow,
+    Colors.orange,
+    Colors.deepOrange,
+    Colors.pink,
+    Colors.black,
+  ];
 
   Project project = Project.nullProject.copyWith(); // 생성되는 프로젝트
   int currentPage = 0; // 현재 페이지
+
   Color seedColor = $style.colors.primary; // 시드 컬러
   ColorScheme get colorScheme => ColorScheme.fromSeed(seedColor: seedColor); // 시드 호환 컬러
+
   late Rx<XFile> mainImgXFile = XFile(project.imgPath).obs; // 헤더 이미지
+
   RxBool descriptionsIsOk = false.obs; // description isOk
+  List<XFile> descriptionXFileList = []; // description 이미지 리스트
 
   String callbackType = ''; // 콜백 타입
   List<String> detailCallbackTypeList = []; // 디테일 콜백 타입 리스트
+  RxBool callToActionIsOk = false.obs; // callToAction isOk
 
   @override
   void onClose() {
@@ -47,20 +66,49 @@ class CreateProjectController extends GetxController {
     pageController.nextPage(duration: $style.times.ms300, curve: Curves.easeIn);
   }
 
+  // 이전 질문
+  void previousQuestion() {
+    if (currentPage > 0) {
+      pageController.previousPage(duration: $style.times.ms300, curve: Curves.easeIn);
+    } else {
+      onWillPop();
+    }
+  }
+
+  Future<bool> onWillPop() async {
+    late bool value;
+    await GlobalFunction.showCustomDialog(
+        title: '정말 종료하시겠어요?',
+        description: '작성하던 내용들이 모두 사라져요.',
+        showCancelBtn: true,
+        okText: '계속 만들기',
+        okFunc: () {
+          value = false;
+          Get.close(1); // 다이얼로그
+        },
+        cancelText: '종료',
+        cancelFunc: () {
+          value = true;
+          if (Get.previousRoute == Routes.home) {
+            Get.close(2); // 다이얼로그, 현재 페이지
+          } else {
+            Get.offAllNamed(Routes.home);
+          }
+        });
+
+    return value;
+  }
+
   // 사진 가져오기
   Future<XFile?> getImage() async {
-    final ImagePicker picker = ImagePicker();
     XFile? selectedImage;
 
-    // selectedImage = await picker.pickImage(source: ImageSource.gallery);
     FilePickerResult? pickedFileWeb = await FilePicker.platform.pickFiles(type: FileType.image);
-    
+
     if (pickedFileWeb != null) {
       Uint8List? fileBytes = pickedFileWeb.files.first.bytes;
 
-      selectedImage = XFile.fromData(fileBytes!);
-
-      update(['img']);
+      selectedImage = XFile.fromData(fileBytes!, name: pickedFileWeb.files.first.name);
     }
 
     return selectedImage;
@@ -98,9 +146,9 @@ class CreateProjectController extends GetxController {
   }
 
   // descriptions isOk 체크
-  void descriptionsIsOkCheck(){
-    for(Description description in project.descriptions) {
-      if(description.title.isEmpty || description.contents.isEmpty || description.imgPath.isEmpty) {
+  void descriptionsIsOkCheck() {
+    for (Description description in project.descriptions) {
+      if (description.title.isEmpty || description.contents.isEmpty || description.imgPath.isEmpty) {
         descriptionsIsOk(false);
         return;
       }
@@ -110,7 +158,7 @@ class CreateProjectController extends GetxController {
   }
 
   // 콜투 액션 타입 세팅
-  void setCallbackType(String type){
+  void setCallbackType(String type) {
     callbackType = type;
     detailCallbackTypeList.clear();
 
@@ -119,33 +167,125 @@ class CreateProjectController extends GetxController {
 
   // 콜투 액션 디테일 타입 세팅
   void setDetailCallbackType(String type, bool isContain) {
-    if(detailCallbackTypeList.isEmpty) {
+    if (detailCallbackTypeList.isEmpty) {
       detailCallbackTypeList.add(type);
     } else {
-      if(isContain) {
+      if (isContain) {
         detailCallbackTypeList.remove(type);
       } else {
-        if(type.isNotEmpty) detailCallbackTypeList.add(type);
+        if (type.isNotEmpty) detailCallbackTypeList.add(type);
       }
     }
 
-    // print(detailCallbackTypeList);
     update(['callToAction']);
   }
 
   // call to action isOk
-  bool callToActionIsOk(){
-    if(callbackType.isEmpty) return false;
+  void callToActionOkCheck() {
+    if (callbackType.isEmpty) callToActionIsOk(false);
 
-    switch(callbackType) {
+    switch (callbackType) {
       case UserCallback.typeNone:
-        return detailCallbackTypeList.isEmpty;
+        callToActionIsOk(detailCallbackTypeList.isEmpty);
       case UserCallback.typeForm:
-        return detailCallbackTypeList.isNotEmpty;
+        callToActionIsOk(detailCallbackTypeList.isNotEmpty);
       case UserCallback.typeLink:
-        return detailCallbackTypeList.isNotEmpty;
+        callToActionIsOk(detailCallbackTypeList.isNotEmpty);
       default:
-        return false;
+        callToActionIsOk(false);
     }
+  }
+
+  // callbackType 조립
+  String assemblyCallbackType() {
+    if (callbackType == UserCallback.typeNone) return callbackType;
+    String detailType = '';
+
+    for (int i = 0; i < detailCallbackTypeList.length; i++) {
+      final String detail = detailCallbackTypeList[i];
+
+      if (i < detailCallbackTypeList.length - 1) {
+        detailType += '$detail$formDivision';
+      } else {
+        detailType += detail;
+      }
+    }
+
+    return '$callbackType$division$detailType';
+  }
+
+  // 키 컬러 변경
+  void onChangedKeyColor(Color color) {
+    seedColor = color;
+    project.keyColor = color.value;
+
+    update();
+  }
+
+  // 프로젝트 생성
+  Future<void> createProject() async {
+    GlobalFunction.loadingDialog(); // 로딩 시작
+
+    // 메인 이미지 등록
+    final String? mainImageUrl = await imgUpload(mainImgXFile.value);
+
+    List<String> descriptionImageUrlList = []; // description 이미지 url 리스트
+
+    if (mainImageUrl != null) {
+      // description 이미지 등록
+      for (XFile xFile in descriptionXFileList) {
+        final String? imageUrl = await imgUpload(xFile);
+
+        if (imageUrl != null) {
+          descriptionImageUrlList.add(imageUrl);
+        } else {
+          Get.close(1); // 로딩 끝
+
+          // description 이미지 등록 실패 시 리턴
+          return GlobalFunction.showToast(msg: '이미지 등록 실패');
+        }
+      }
+    } else {
+      Get.close(1); // 로딩 끝
+
+      // 메인 이미지 등록 실패 시 리턴
+      return GlobalFunction.showToast(msg: '이미지 등록 실패');
+    }
+
+    project.imgPath = mainImageUrl; // 메인 이미지 url 설정
+
+    // description 이미지 url 설정
+    for (int i = 0; i < descriptionImageUrlList.length; i++) {
+      project.descriptions[i].imgPath = descriptionImageUrlList[i];
+    }
+    project.callbackType = assemblyCallbackType(); // 콜백 타입 설정
+
+    // 프로젝트 업로드
+    Project? res = await ProjectRepository.createProject(project);
+
+    if (res != null) {
+      Get.close(1); // 로딩 끝
+      // 결과물 페이지로 이동
+    } else {
+      Get.close(1); // 로딩 끝
+      GlobalFunction.showToast(msg: '잠시후 다시 시도해 주세요.');
+    }
+  }
+
+  // 이미지 업로드
+  Future<String?> imgUpload(XFile xFile) async {
+    String? url;
+
+    try {
+      Uint8List? fileBytes = await xFile.readAsBytes();
+      String fileName = '${DateTime.now().millisecondsSinceEpoch}.${xFile.name.split('.').last}';
+
+      TaskSnapshot uploadTask = await FirebaseStorage.instance.ref('project/$fileName').putData(fileBytes);
+      url = await uploadTask.ref.getDownloadURL();
+    } catch (e) {
+      if (kDebugMode) print(e);
+    }
+
+    return url;
   }
 }
