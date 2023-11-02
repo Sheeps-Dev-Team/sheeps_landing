8faf491c8c0e6abec721dc9cf345c064c5994506
Eden0622 <84838438+Eden0622@users.noreply.github.com>
2023-11-02 16:42:45 +0900

    프로젝트 관리 페이지 디자인 변경

--- lib/util/global_function.dart
+++ lib/util/global_function.dart
@@ -231,7 +231,7 @@ class GlobalFunction {
   // 뒤로가기 (이전 라우트가 없으면 홈으로가기)
   static void goToBack(){
     if (Get.previousRoute.isEmpty) {
-      Get.offAllNamed(Routes.home);
+      Get.offAllNamed(Routes.index);
     } else {
       Get.back();
     }
